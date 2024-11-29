63bc5bad1bb3215c9c06864d5f7969c624ea9d99
JudithFogacho <judith.fogacho@udla.edu.ec>
2024-11-29 07:43:25 -0500

    Imagenes añadidas y creacion de about page

---
+++ AFAboutPage.xaml.cs
@@ -0,0 +1,16 @@
+namespace FogachoApuntes;
+
+public partial class AFAboutPage : ContentPage
+{
+	public AFAboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+
+    }
+}
